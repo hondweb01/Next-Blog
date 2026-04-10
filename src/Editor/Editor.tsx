26ff4017561c195c9ebd0e395be36892ecc32b76
Your Name <you@example.com>
2026-04-11 03:47:01 +0900

    test

--- src/Editor/Editor.tsx
+++ src/Editor/Editor.tsx
@@ -63,7 +63,7 @@ for (const item of items) {
     }
 
    //res.url は「APIのレスポンスボディ」ではない、Responseオブジェクト (リクエストしたurl)
-   //mdの形式で表示
+   //mdの形式で表示する
     const data = await res.json();
     cm.replaceSelection(`![](${data.url})`);
   }
